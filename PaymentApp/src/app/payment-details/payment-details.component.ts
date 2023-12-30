a57d75a85053ab05ec15a3f1b73b85e271478489
Lufeltz <lufelt@proton.me>
2023-12-30 19:25:35 -0300

    feat: Inicializar serviço PaymentDetail

--- PaymentApp/src/app/payment-details/payment-details.component.ts
+++ PaymentApp/src/app/payment-details/payment-details.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from '../shared/payment-detail.service';
 
 @Component({
   selector: 'app-payment-details',
   templateUrl: './payment-details.component.html',
   styles: ``
 })
-export class PaymentDetailsComponent {
+export class PaymentDetailsComponent implements OnInit {
 
+  constructor(public service: PaymentDetailService) { }
+
+  ngOnInit(): void {
+    this.service.refreshList();
+  }
 }
