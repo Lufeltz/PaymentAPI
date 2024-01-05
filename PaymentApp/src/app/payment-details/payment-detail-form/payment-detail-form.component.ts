7a68aa41849609953ab4becabb5dfbc045459f5e
Lufeltz <lufelt@proton.me>
2024-01-04 23:57:45 -0300

    feat: Adicionar método updateRecord para alterações
    Além do método updateRecord a requisição post anterior foi movida para o método insertRecord

--- PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -15,17 +15,38 @@ export class PaymentDetailFormComponent {
   onSubmit(form: NgForm) {
     this.service.formSubmitted = true;
     if (form.valid) {
-      this.service.postPaymentDetail()
-        .subscribe({
-          next: res => {
-            this.service.list = res as PaymentDetail[];
-            this.service.resetForm(form);
-            this.toastr.success("Inserted successfully", "Payment Detail Register");
-          },
-          error: err => {
-            console.log(err);
-          }
-        })
+      if(this.service.formData.paymentDetailId == 0)
+        this.insertRecord(form);
+      else
+        this.updateRecord(form);
+      
     }
+  }
+
+  insertRecord(form: NgForm) {
+    this.service.postPaymentDetail()
+      .subscribe({
+        next: res => {
+          this.service.list = res as PaymentDetail[];
+          this.service.resetForm(form);
+          this.toastr.success("Inserted successfully", "Payment Detail Register");
+        },
+        error: err => {
+          console.log(err);
+        }
+      })
+  }
+  updateRecord(form: NgForm) { 
+    this.service.putPaymentDetail()
+      .subscribe({
+        next: res => {
+          this.service.list = res as PaymentDetail[];
+          this.service.resetForm(form);
+          this.toastr.info("Updated successfully", "Payment Detail Register");
+        },
+        error: err => {
+          console.log(err);
+        }
+      })
   }
 }
